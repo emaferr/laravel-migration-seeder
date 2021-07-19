96448d21bddbc03827511a9c2b14409df8c58a2d
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-07-19 18:14:36 +0200

    Create project Install Laravel Bootstrap amount Create migration model and controller Create travels table Inseminated database using seeders Cycle within the db

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $travels= App\Travel::all();
+    return view('welcome',compact('travels'));
 });
